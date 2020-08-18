35120dac21f1b43694ebf0a2698a4f3213d25199
Jinjae Lee <leejinjae7@gmail.com>
2020-08-18 21:28:43 +0900

    커밋

--- web/server.py
+++ web/server.py
@@ -7,8 +7,11 @@ import datetime
 image = None
 
 app = Flask(__name__)
+app.secret_key = 'app secret key'
 
 ###################################################modules
+
+
 
 def yellow(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -196,13 +199,36 @@ def purple(image):
 
     return dst
 
+def brown(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = (0,80, 80)
+    upper= (13, 255, 255)
+
+    mask = cv2.inRange(hsv, lower, upper)
+
+    mask = cv2.medianBlur(mask, 9)
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
+    mask = cv2.dilate(mask, kernel, iterations=1)
+
+    obj = cv2.bitwise_and(image, image, mask=mask)
+    dst = cv2.addWeighted(gray, 0.5, obj, 1.0, 0.0)
+
+    return dst
 
 
 ###########################################################apps
 
 @app.route('/')
 def index():
-    return render_template("imageprocessing.html", ctx={"title":"Over the Rainbow"})
+    if 'counter' in session:
+        session['counter'] += 1
+    else:
+        session['counter'] = 1
+
+    return render_template("index.html", ctx={"title":"Over the Rainbow"})
 
 
 @app.route('/upload', methods=["POST"]) 
@@ -216,8 +242,8 @@ def upload():
     print(image.shape)
 #     session["image"] = image
 
-    return redirect("/")
-
+    # return redirect("/")
+    return render_template("imageprocessing.html", ctx={"title":"Over the Rainbow"})
 
 @app.route('/imageprocess')
 def imageprocss():
